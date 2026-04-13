668f8e21e60a305a3e625cf0ed747562c141cffe
Wagen666 <abtdomain@outlook.com>
2026-04-13 21:34:54 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,8 @@
 project = 'dksplit'
 copyright = '2026, ABTdomain'
 author = 'ABTdomain'
-version = '0.2.3'
-release = '0.2.3'
+version = '0.3.1'
+release = '0.3.1'
 
 extensions = ['myst_parser']
 html_theme = 'sphinx_rtd_theme'
